cc45132fb482b5d47346bf8fcd17dd35767a2519
Peabrain <peabrainc64@hotmail.com>
2016-03-23 23:49:52 +0100

    h

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,18 +3,22 @@
 #include	<proto/intuition.h>
 #include	<proto/dos.h>
 #include	<stdio.h>
+#include	<exec/io.h>
+#include	<exec/types.h>
+#include	<time.h>
 #include	"screen.h"
 #include	"input.h"
 #include	"c2p.h"
 #include	"texture.h"
 
+double	clockToMilliseconds(clock_t ticks)
+{
+	return (ticks/(double)CLOCKS_PER_SEC) / 1000.0;
+}
 
 int main()
 {
-	SystemControl(
-	SCON_TakeOverSys,TRUE,
-	SCON_StopInput,TRUE
-	);
+	SystemControl(SCON_TakeOverSys,TRUE,SCON_StopInput,TRUE);
 
 	SCREEN	*myScreen = 0;
 	C2P		*myC2P = 0;
@@ -32,9 +36,21 @@ int main()
 		myTexture = new TEXTURE();
 		myTexture->Load("Wall.raw");
 
-		int h = 0;
+		int	xPosOrg = 0;
+		int	xOrgadd = (int)(4.0f * 256);
+
+		clock_t beginFrame = clock();
+
 		while(1)
 		{
+			clock_t nextFrame = clock();
+			double diffFrame = difftime(nextFrame,beginFrame);
+			beginFrame = nextFrame;
+			printf("%f\n",diffFrame);
+
+			xOrgadd = (int)(256.0 * diffFrame);
+
+
 			unsigned char *CnkBuffer = myC2P->GetBuffer();
 			unsigned int Width = myC2P->GetWidth();
 			unsigned int Height = myC2P->GetHeight();
@@ -43,21 +59,24 @@ int main()
 			int	TexWidth = myTexture->GetWidth();
 			int	TexHeight = myTexture->GetHeight();
 
+			int	xadd = 256 * 96 / 160;
+			int	xpos = 0;
 			for(int j = 0;j < Width;j++)
 			{
-				unsigned char *p = TexBuf + ((j + h) % TexHeight) * TexWidth;
-				int	add = 256 * 96 / 160;
-				int	pos = 0;
+				unsigned char *p = TexBuf + (((xpos + xPosOrg) >> 8) % TexHeight) * TexWidth;
+				int	yadd = 256 * 96 / 160;
+				int	ypos = 0;
 				for(int i = 0;i < Height;i++)
 				{
-					CnkBuffer[i * Width + j] = p[pos >> 8];
-					pos += add;
+					CnkBuffer[i * Width + j] = p[ypos >> 8];
+					ypos += yadd;
 				}
+				xpos += xadd;
 			}
 
 			myC2P->Convert((unsigned int)myScreen->GetBitPlane(0) + 40 * 8 * (256 / 2 - Height / 2 - 32) + 8);
 
-			h += 4;
+			xPosOrg += xOrgadd;
 
 			if(isLeftMouseButtonPressed()) break;
 		}
